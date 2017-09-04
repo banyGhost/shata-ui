2d84a58b2cd9ba10f9699993493719162e9468c4
Bert <boyrlf@sian.com>
2017-09-04 17:40:36 +0800

    csstoless

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,14 @@
 var gulp = require('gulp'),
-  minifycss = require('gulp-minify-css'),
-  concat = require('gulp-concat'),
-  rename = require('gulp-rename'),
+  postcss = require('gulp-postCss'),
+  autoprefixer = require('autoprefixer'),
+  cssnext = require('cssnext'),
   less = require('gulp-less'),
-  connect = require('gulp-connect'),
-  notify = require('gulp-notify');
+  precss = require('precss');
 
-// less to css
-gulp.task('less2css', function() {
+gulp.task('less', function () {
+  var processors = [autoprefixer, cssnext, precss];
   return gulp.src('./src/less/*.less')
+    .pipe(postcss(processors))
     .pipe(less())
     .pipe(gulp.dest('./src/css'))
-    .pipe(notify({ message: 'less2css task ok' }));
-});
-
-gulp.task('css', function() {
-  return gulp.src('src/css/*.css')
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('./dest/css'))
-    .pipe(rename({ suffix: '.min' }))
-    // .pipe(minifycss())
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(notify({ message: 'css task ok' }));
-});
-
-// 默认任务
-gulp.task('default', function() {
-
-  gulp.run('less2css', 'css');
-
 });
